942d7bcfe6598c2bc1d2f957c946cd10c5da2719
Ilya Ovdin <iovdin@gmail.com>
2025-06-14 10:26:25 +0200

    make log save full payload with headers etc

--- tools/log.proc.js
+++ tools/log.proc.js
@@ -3,7 +3,9 @@ module.exports = async (node, args, ctx)  => {
   const newNode = Object.assign({}, node);
   newNode.exec = async function(payload, ctx) {
     const res = await node.exec(payload, ctx)
-    fs.writeFileSync(args.trim() || "log.json", JSON.stringify(JSON.parse(res.body), null, "  "));
+    const body = JSON.parse(res.body)
+    payload = {...res, body};
+    fs.writeFileSync(args.trim() || "log.json", JSON.stringify(payload, null, "  "));
     return res
   }
   return newNode
