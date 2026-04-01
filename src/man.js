36ff03d585d7b1930ffaa684990bc91045850ccf
Ilya Ovdin <iovdin@gmail.com>
2026-04-01 11:53:23 +0200

    feat: Migrate manual logic to globalThis
    Refactored the manual handling to use `globalThis` for better isolation and consistency across multiple package installations. Added regex matching for manual retrieval.

--- src/man.js
+++ src/man.js
@@ -3,8 +3,15 @@ const path = require("path")
 // all the registered manuals
 // { name, description, filename/content } 
 
-let mans = [
-] 
+
+// i have to make to keep mans in globalThis because keeping it in module context
+// breaks when few copies of tune-sdk is installed
+let globalKey = "tune.man";
+
+function getStore() {
+  globalThis[globalKey] ||= []
+  return globalThis[globalKey]
+}
 
 /*
   @man include all manuals for all packages included
@@ -12,12 +19,15 @@ let mans = [
   @man/tune - get manual for tune core package 
   @man/tune-basic-toolset - get manual for tune-basic-tool set package
 
-  TODO:
+  TODO: subdocuments
   @man/tune/js-api - lis
   @man/tune/agents - lis
 */
 
-function read({ filename, content }) {
+function read(man) {
+  const store = getStore()
+
+  const { filename, content } = man
   if (content) {
     return content
   }
@@ -27,56 +37,115 @@ function read({ filename, content }) {
   return fs.readFileSync(filename)
 }
 
-module.exports = ({ mount } = {}) => async (name, { type }) => {
-  if ((type || "any") !== 'any' && type !== 'text' ) {
-    return
-  }
+module.exports = ({ mount } = {}) => {
   mount ||= "man"
+  return async (name, { type, match, output }) => {
+    if ((type || "any") !== 'any' && type !== 'text' ) {
+      return
+    }
 
-  // supported only with mount
-  if (!mount) {
-    return 
-  }
+    // supported only with mount
+    if (!mount) {
+      return 
+    }
 
-  // @man include all the manuals
-  if (name === mount) {
+    const mans = getStore()
+
+    if (match === "regex") {
+      re = new RegExp(name);
+      const result = mans.filter(man => re.test(`${mount}/${man.name}`)).map(man => ({
+        type: "text",
+        source: "docs",
+        name: `${mount}/${man.name}`,
+        read: async () => read(man) 
+      }))
+
+      // @man/
+      if (re.test(`${mount}/`)) {
+        result.unshift({
+          type: "text",
+          name: `${mount}/`,
+          source: "docs",
+          read: async () => mans.map(man => `${man.name} - ${man.description}`).join("\n")
+        })
+      }
+
+      // @man 
+      if (re.test(mount)) {
+        result.unshift({
+          type: "text",
+          name: mount,
+          source: "docs",
+          read: async () => mans.map(man => `<${man.name}>\n ${read(man)} \n</${man.name}>`).join("\n")
+        })
+      }
+
+
+      if (!result.length) {
+        return
+      }
+      if (output === "all") {
+        return result
+      }
+      return result[0]
+    }
+
+    // @man include all the manuals
+    if (name === mount) {
+      return ({
+        type: "text",
+        name,
+        read: async () => mans.map(man => `<${man.name}>\n ${read(man)} \n</${man.name}>`).join("\n")
+      })
+    }
+
+    if (!name.startsWith(mount + '/')) {
+      return 
+    }
+
+    // @man/ - list all the manuals to include
+    if (name === mount + '/') {
+      return {
+        type: "text",
+        name,
+        read: async () => mans.map(man => `${man.name} - ${man.description}`).join("\n")
+      }
+    }
+
+
+    // @man/tune - get manual for tune core package 
+    // @man/tune-basic-toolset - get manual for tune-basic-tool set package
+
+    const actualName = name.slice(mount.length + 1);
+
+    const man = mans.find(man => man.name === actualName);
+    if (!man) return
+
     return {
       type: "text",
       name,
-      read: async () => mans.map(man => `<${man.name}>\n ${read(man)} \n</${man.name}>`).join("\n")
+      read: async() => read(man)
     }
-  }
-
-  if (!name.startsWith(mount + '/')) {
-    return 
-  }
-
-  // @man/ - list all the manuals to include
-  if (name === mount + '/') {
-    return {
-      type: "text",
-      name,
-      read: async () => mans.map(man => `${man.name} - ${man.description}`).join("\n")
-    }
-  }
-
-
-  // @man/tune - get manual for tune core package 
-  // @man/tune-basic-toolset - get manual for tune-basic-tool set package
-  
-  const actualName = name.slice(mount.length + 1);
-
-  const man = mans.find(man => man.name === actualName);
-  if (!man) return
-
-  return {
-    type: "text",
-    name,
-    read: async() => read(man.filename)
   }
 }
 
-const addManual = (dirname) => {
+const add = (man) => {
+  const {name, description, content, filename} = man
+  if (!content && !filename) {
+    throw Error(`neither content nor filename is set for '${name}' manual`)
+  }
+
+  const mans = getStore() 
+
+  let idx = mans.findIndex(man => man.name === name)
+  if (idx !== -1) {
+    mans.splice(idx, 1, man)
+  } else {
+    mans.push(man)
+  }
+}
+
+const addPackage = (dirname) => {
   let package = path.resolve(dirname, "package.json");
   if (!fs.existsSync(package)){
     dirname = path.resolve(dirname, "..")
@@ -89,29 +158,14 @@ const addManual = (dirname) => {
 
   const { name, version, description } = JSON.parse(fs.readFileSync(package, "utf8"));
 
-  const newMans = mans.filter(man => man.name !== name)
-
   const filename = path.resolve(dirname, "README.md")
+  add({ name, version, description, filename })
 
-  newMans.push({name, description, filename})
-  mans = newMans
 }
 
-addManual(__dirname)
-/*
-const addManual = ({name, description, content, filename}) => {
-  const  = params;
+// add tune-sdk package
+addPackage(__dirname)
 
-  const newMans = mans.filter(man => man.name !== name)
-  if (!name) {
-    throw Error(`name is not set for manual`)
-  }
-  if (!content && !filename) {
-    throw Error(`neither content nor filename is set for '${name}' manual`)
-  }
 
-  newMans.push({name, description, content, filename})
-}
-*/
-
-module.exports.addManual = addManual;
+module.exports.add = add;
+module.exports.addPackage = addPackage;
